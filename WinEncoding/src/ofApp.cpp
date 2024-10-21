b31c6a04d28bfd0c0800ea233c9521ff6718281c
Dimitre <dimitre@users.noreply.github.com>
2024-10-20 23:31:13 -0300

    update

--- WinEncoding/src/ofApp.cpp
+++ WinEncoding/src/ofApp.cpp
@@ -26,17 +26,32 @@ void ofApp::keyPressed(int key){
 	if (key == 'a') {
 		ofSystemAlertDialog("Translação, rotação");
 	}
-	else if (key == 'd') {
+	else if (key == 's') {
 		auto s = ofSystemTextBoxDialog("Anunciação what is your favorite color?" , "Anunciação blue");
 	}
-	else if (key == 'l') {
+	else if (key == 'd') {
 		ofFileDialogResult result = ofSystemLoadDialog("Anunciação Load file", true, "folderzão");
 		if(result.bSuccess) {
 			image.load(result.getPath());
-//			path = result.getPath().u8string();
-//			image.load(result.getPath());
 			cout << result.getPath() << endl;
-		  // load your file at `path`
+			wcout << result.getPath() << endl;
 		}
 	}
+	else if (key == 'e') {
+		ofFileDialogResult result = ofSystemLoadDialog("Anunciação Load file", false, "folderzão");
+		if(result.bSuccess) {
+			image.load(result.getPath());
+			cout << result.getPath() << endl;
+			wcout << result.getPath() << endl;
+		}
+	}
+	else if (key == 'f') {
+		ofFileDialogResult result = ofSystemLoadDialog("Anunciação Load file");
+		if(result.bSuccess) {
+			image.load(result.getPath());
+			cout << result.getPath() << endl;
+			wcout << result.getPath() << endl;
+		}
+	}
+
 }
